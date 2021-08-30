c0881de88a2e0b691b6bdfd03938f0bdd552f68e
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-08-30 11:31:25 +0200

    feat: error message flash

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
+let session = require('express-session');
+const { response } = require('express');
 
 // Template engine
 app.set('view engine', 'ejs');
@@ -9,14 +11,23 @@ app.set('view engine', 'ejs');
 app.use('/assets', express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+app.use(session({
+  secret: 'azerty', // I know this is not secure it's juste for test
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(require('./middlewares/flash'));
 
 // Routes
 app.get('/', (request, response) => {
-  response.render('pages/index', {test :'Salut'});
+  console.log(request.session);
+  response.render('pages/index');
 });
 
 app.post('/', (request, response) => {
   if (request.body.message === undefined || request.body.message === '') {
+    request.flash('error', "Vous n'avez pas posté de message.")
     response.redirect('/');
   };
 });
